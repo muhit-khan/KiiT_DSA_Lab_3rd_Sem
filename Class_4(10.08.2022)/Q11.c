2548af31c5098af5c202937ded2f7cc343b159f5
Muhit Khan <muhitkhan_bd@outlook.com>
2022-08-18 19:59:04 +0530

    Update Q11.c

--- Class_4(10.08.2022)/Q11.c
+++ Class_4(10.08.2022)/Q11.c
@@ -6,27 +6,30 @@
 #include<stdlib.h>
 int main() {
     int n, count=0;
-    int *data, *restruct, large = 0.00;
+    int *data, *restruct;
     printf("Enter the total number of elements: ");
     scanf("%d", &n);
     // Allocating memory for n elements
-    data = (int *)mcalloc(n * sizeof(int));
-    restruct = (int *)mcalloc(n * sizeof(int));
+    data = (int *)malloc(n * sizeof(int));
+    restruct = (int *)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         printf("Enter number%d: ", i + 1);
-        scanf("%d", data + i);
+        scanf("%d", (data + i));
     }
     int p = 0, q = n-1;
-    for (int i = 0; i < n; ++i){
-        if (*(data+i)%2 == 0){
-            *(restruct+p) = *(data+1);
+    for (int i = 0; i < n; i++){
+        if (*(data+i)%2 == 0)
+        {
+            *(restruct+p) = *(data+i);
             p += 1;
         }
         else
-            *(restruct+q) = *(data+1);
+            *(restruct+q) = *(data+i);
             q -= 1;
     }
-    printf("\nLargest number presents %d times", count);
-    free(data);
+    printf("\nModified Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", *(restruct+i));
+    }
     return 0;
 }
