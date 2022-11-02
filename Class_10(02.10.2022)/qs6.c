f9f4802e691f6326032ae7827ba584bf75533565
Muhit Khan <muhitkhan_bd@outlook.com>
2022-11-02 13:53:30 +0530

    Create qs6.c

---
+++ Class_10(02.10.2022)/qs6.c
@@ -0,0 +1,102 @@
+/*Q1.To count number of leaf nodes in the tree.*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <malloc.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *tree;
+void create_tree(struct node *);
+struct node *insertElement(struct node *, int);
+int totalNodes(struct node *);
+int totalExternalNodes(struct node *);
+int main()
+{
+    int option, val;
+    struct node *ptr;
+    create_tree(tree);
+    do
+    {
+        printf("\n *****MAIN MENU****** \n");
+        printf("\n 1. Insert Element");
+        printf("\n 2. Count the total number of nodes");
+        printf("\n 3. Count the total number of external nodes");
+        printf("\n 4. Exit");
+        printf("\n\n Enter your option : ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            printf("\n Enter the value of the new node : ");
+            scanf("%d", &val);
+            tree = insertElement(tree, val);
+            break;
+        case 2:
+            printf("\n Total no. of nodes = %d", totalNodes(tree));
+            break;
+        case 3:
+            printf("\n Total no. of external nodes = %d",
+                   totalExternalNodes(tree));
+            break;
+        }
+    } while (option != 4);
+    getch();
+    return 0;
+}
+void create_tree(struct node *tree)
+{
+    tree = NULL;
+}
+struct node *insertElement(struct node *tree, int val)
+{
+    struct node *ptr, *nodeptr, *parentptr;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = val;
+    ptr->left = NULL;
+    ptr->right = NULL;
+    if (tree == NULL)
+    {
+        tree = ptr;
+        tree->left = NULL;
+        tree->right = NULL;
+    }
+    else
+    {
+        parentptr = NULL;
+        nodeptr = tree;
+        while (nodeptr != NULL)
+        {
+            parentptr = nodeptr;
+            if (val < nodeptr->data)
+                nodeptr = nodeptr->left;
+            else
+                nodeptr = nodeptr->right;
+        }
+        if (val < parentptr->data)
+            parentptr->left = ptr;
+        else
+            parentptr->right = ptr;
+    }
+    return tree;
+}
+int totalNodes(struct node *tree)
+{
+    if (tree == NULL)
+        return 0;
+    else
+        return (totalNodes(tree->left) + totalNodes(tree->right) + 1);
+}
+int totalExternalNodes(struct node *tree)
+{
+    if (tree == NULL)
+        return 0;
+    else if ((tree->left == NULL) && (tree->right == NULL))
+        return 1;
+    else
+        return (totalExternalNodes(tree->left) +
+                totalExternalNodes(tree->right));
+}
